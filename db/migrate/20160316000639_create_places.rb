485572d46114dfd59091cf33213d04d5c0620226
Gary Marx <gary@zenpayroll.com>
2016-03-15 17:07:01 -0700

    new branch!

---
+++ db/migrate/20160316000639_create_places.rb
@@ -0,0 +1,10 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+      t.string :address
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
